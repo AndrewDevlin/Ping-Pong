5e7f9fca14e15318d9af168841e120e0078c3bd5
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-01-20 12:43:13 -0800

    add basic front-end logic to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//back-end lagic
+
+
+//front-end logic
+$(document).ready(function() {
+  $("#ping-pong.btn")submit(function(event) {
+    event.preventDefault();
+    $("#results").text();
+  });
+});
