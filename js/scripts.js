355fe49e96b18e4ca886aa720e61e1352e62043f
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-01-20 20:42:29 -0800

    clean up scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    //back-end lagic
+
     var inputNum = $("#user-number").val();
-    var numbers = []
     var output = []
 
-  for (var index = 1; index <= inputNum; index += 1)
-    numbers = numbers + index
-
-  for (var index = 1; index <= numbers.length; index += 1)
-    if (index % 3 === 0 && index % 5 !== 0) {
-       output = output + "ping"
+    for (var index = 1; index <= inputNum; index += 1)
+      if (index % 3 === 0 && index % 5 !== 0) {
+        output = output + "ping"
       }
       else if (index % 5 === 0 && index % 3 !== 0) {
         output = output + "pong"
@@ -22,9 +18,6 @@ $(document).ready(function() {
       else {
         output = output + index
       }
-
-
-    //front-end logic
 
     $("#results p").text(output);
   });
