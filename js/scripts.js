8a97d2b2c23b1f215737b8d38d9a3ae1f2e80f7f
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-01-24 12:55:03 -0800

    add back-end logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
-  $("form").submit(function(event) {
-    event.preventDefault();
+//back-end logic
+var output = ''
+//alert('p')
+var makeList = function(num) {
+  if (num < 1) {
+    alert('Please enter a nuber >= 0')
+  }
+  else {
+    for (var index = 1; index <= num; index += 1) {
 
-
-    var inputNum = $("#user-number").val();
-    var output = ''
-
-    for (var index = 1; index <= inputNum; index += 1)
       if (index % 3 === 0 && index % 5 !== 0) {
         output = output + "<span class=ping>" + "ping," + "</span>"
       }
@@ -19,6 +20,34 @@ $(document).ready(function() {
       else {
         output = output + index + ","
       }
+    }
+  }    //alert('p')
+};
+
+//front-end logic
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    //alert(k)
+    var inputNum = $("#user-number").val();
+    makeList(inputNum)
+
+    //var output = ''
+
+    //for (var index = 1; index <= inputNum; index += 1)
+      //if (index % 3 === 0 && index % 5 !== 0) {
+        //output = output + "<span class=ping>" + "ping," + "</span>"
+      //}
+      //else if (index % 5 === 0 && index % 3 !== 0) {
+        //output = output + "<span class=pong>" + "pong," + "</span>"
+      //}
+      //else if (index % 15 === 0) {
+        //output = output + "<span class=ping-pong>" + "ping-pong," + "</span>"
+      //}
+      //else {
+        //output = output + index + ","
+      //}
 
     output = output.split(',');
 
