443396f7c263a266fecd21b1b670e639cfd08b3c
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-01-26 20:47:46 -0800

    add color to the words in output

--- js/scripts.js
+++ js/scripts.js
@@ -20,14 +20,25 @@ var makeList = function(num) {
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    $('#results').empty();
+    $("#results").empty();
     var inputNum = $("#user-number").val();
     if (inputNum < 1) {
-      alert('Please enter a number > 0')
+      alert("Please enter a number > 0")
     }
     var output = makeList(inputNum)
     output.forEach(function(number) {
-      $("#results").append("<li>" + number + "</li>");
+      if (number === "ping-pong") {
+        $("#results").append("<li>" + "<span style='color:purple'>" + number + "</span>" + "</l>")
+      }
+      else if (number === "ping") {
+        $("#results").append("<li>" + "<span style='color:red'>" + number + "</span>" + "</l>")
+      }
+      else if (number === "pong") {
+        $("#results").append("<li>" + "<span style='color:blue'>" + number + "</span>" + "</l>")
+       }
+      else {
+        $("#results").append("<li>" + number + "</li>");
+      }
     })
   });
 });
