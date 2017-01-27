43a574393ac35e44d5a8effbeba30ee1d8694b61
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-01-26 19:15:06 -0800

    move the for alert for entries les then 1 the user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-//back-end logic
-var output = ''
-//alert('p')
 var makeList = function(num) {
+  var output = ''
   if (num < 1) {
     alert('Please enter a nuber >= 0')
   }
@@ -21,37 +19,19 @@ var makeList = function(num) {
         output = output + index + ","
       }
     }
-  }    //alert('p')
+  }
+  return output
 };
 
 //front-end logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-
-    //alert(k)
+    $('#results').empty();
     var inputNum = $("#user-number").val();
-    makeList(inputNum)
-
-    //var output = ''
-
-    //for (var index = 1; index <= inputNum; index += 1)
-      //if (index % 3 === 0 && index % 5 !== 0) {
-        //output = output + "<span class=ping>" + "ping," + "</span>"
-      //}
-      //else if (index % 5 === 0 && index % 3 !== 0) {
-        //output = output + "<span class=pong>" + "pong," + "</span>"
-      //}
-      //else if (index % 15 === 0) {
-        //output = output + "<span class=ping-pong>" + "ping-pong," + "</span>"
-      //}
-      //else {
-        //output = output + index + ","
-      //}
-
+    var output = makeList(inputNum)
     output = output.split(',');
 
-    $('#results').empty();
     for (index = 0; index < output.length; index++) {
       $("#results").append("<li>" + output[index]);
     }
